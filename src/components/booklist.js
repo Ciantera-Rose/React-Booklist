b6650c715d793813b07fefd30bf55f21072f4400
Ciantera-Rose <ciantera.rose@gmail.com>
2020-12-30 22:00:45 -0500

    add Map to iterate over books

--- src/components/booklist.js
+++ src/components/booklist.js
@@ -12,37 +12,40 @@ import React from "react";
 // formatting
 
 //setup vars
-const firstBook = {
-  img: "https://m.media-amazon.com/images/I/71yA4+MC46L._AC_UY218_.jpg",
-  title: "Deep Work: Rules For Focused Success in a Distracted World",
-  author: "Cal Newport",
-};
+const books = [
+  { id: 1,
+    img: "https://m.media-amazon.com/images/I/71yA4+MC46L._AC_UY218_.jpg",
+    title: "Deep Work: Rules For Focused Success in a Distracted World",
+    author: "Cal Newport",
+  },
 
-const secondBook = {
-  img: "https://m.media-amazon.com/images/I/81FGAD1jP0L._AC_UY218_.jpg",
-  title: "Digital Minimalism: Choosing a Focused Life in a Noisy World",
-  author: "Cal Newport",
-};
+  { id: 2,
+    img: "https://m.media-amazon.com/images/I/81FGAD1jP0L._AC_UY218_.jpg",
+    title: "Digital Minimalism: Choosing a Focused Life in a Noisy World",
+    author: "Cal Newport",
+  },
+  
+  { id: 3,
+    img: "https://m.media-amazon.com/images/I/91-C86tJc2L._AC_UY218_.jpg",
+    title: "Algorithms Illuminated: Part 1: The Basics",
+    author: "Tim Roughgarden",
+  },
+];
+
 export default function Booklist() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        return (
+         <Book key={book.id} book={book}></Book>
+        )
+      })}
     </section>
   );
 }
 
-//props destructuring 
 const Book = (props) => {
-  const { img, title, author } = props;
+  const { img, title, author } = props.book
   return (
     <article className="book">
       <img src={img} alt="" />
