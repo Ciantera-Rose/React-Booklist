4fdd947d9de49bad9d4d2609b5adf3b69d3b6dc0
Ciantera-Rose <ciantera.rose@gmail.com>
2020-12-30 20:53:50 -0500

    add destructuring props

--- src/components/booklist.js
+++ src/components/booklist.js
@@ -40,13 +40,14 @@ export default function Booklist() {
   );
 }
 
+//props destructuring 
 const Book = (props) => {
-  console.log(props);
+  const { img, title, author } = props;
   return (
     <article className="book">
-      <img src={props.img} alt="" />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
