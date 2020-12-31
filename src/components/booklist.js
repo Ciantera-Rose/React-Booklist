5569d1e38561bdb418186584cebf0a54780b9437
Ciantera-Rose <ciantera.rose@gmail.com>
2020-12-30 20:46:27 -0500

    add props for  dynamic book

--- src/components/booklist.js
+++ src/components/booklist.js
@@ -11,26 +11,42 @@ import React from "react";
 // close every element
 // formatting
 
+//setup vars
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/71yA4+MC46L._AC_UY218_.jpg",
+  title: "Deep Work: Rules For Focused Success in a Distracted World",
+  author: "Cal Newport",
+};
+
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/81FGAD1jP0L._AC_UY218_.jpg",
+  title: "Digital Minimalism: Choosing a Focused Life in a Noisy World",
+  author: "Cal Newport",
+};
 export default function Booklist() {
   return (
     <section className="booklist">
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
-  const title = 'Humans'
-  const author = "Brandon Stanton";
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51fPR50mdlL._AC_SX184_.jpg"
-        alt=""
-      />
-      <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
-      {/* <p>{}</p> */}
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
